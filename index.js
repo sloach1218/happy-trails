3540052945076948afa2cb5d0abad394e4b93450
Sarah Loach <Sarah@Sarahs-fat-top.attlocal.net>
2019-10-08 23:42:44 -0500

    created basic html form with minimal style & wrote pseudocode outline for javascript

---
+++ index.js
@@ -0,0 +1,19 @@
+//API key for Hiking Project Data API
+const key = '200613378-0612b22d914728adefe17eb3fda22c5e';
+
+//handle user event submit
+
+//get user inputs
+
+//first create query to handle location input
+
+//fetch lat and long coordinates using location input query
+
+//create 2nd query for Hiking Project Data API using location coordinates & user inputs
+
+//fetch trails using 2nd query
+
+//handle & display trail results to user
+
+//clear results if new search submitted
+
